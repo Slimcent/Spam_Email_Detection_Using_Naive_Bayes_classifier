c1eb5e458c64155c8b95951a3485b85793801cd6
Slimcent <centpresley@gmail.com>
2023-10-16 23:32:38 +0100

    Added dataset

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
