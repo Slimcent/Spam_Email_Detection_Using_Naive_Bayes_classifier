f957c4dcc4f286759abde686536970800f3fec4c
Slimcent <centpresley@gmail.com>
2023-10-17 20:13:08 +0100

    logistic method

--- main.py
+++ main.py
@@ -5,3 +5,39 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
+# Load the data from CSV
+data = pd.read_csv('spam_email_dataset.csv')
+
+# Check the first few rows of the dataset
+print(data.head())
+
+# Define the feature (X) and target (y)
+X = data['Email']
+y = data['Spam Indicator']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a CountVectorizer to convert text data to numerical features
+vectorizer = CountVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Initialize and train a Naive Bayes classifier
+nb_classifier = MultinomialNB()
+nb_classifier.fit(X_train, y_train)
+
+# Predict the labels for the test set
+y_pred = nb_classifier.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy * 100:.2f}%')
+
+# Generate a confusion matrix and classification report
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+print("Confusion Matrix:")
+print(confusion)
+print("Classification Report:")
+print(report)
